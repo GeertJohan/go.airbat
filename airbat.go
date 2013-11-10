502c93d483e2f4ba67a7a39c15a70ddda521c41d
GeertJohan <gjr19912@gmail.com>
2013-11-10 16:22:23 +0100

    Initial commit

---
+++ airbat.go
@@ -0,0 +1,66 @@
+package airbat
+
+import (
+	"encoding/base64"
+	"encoding/binary"
+	"errors"
+	"fmt"
+)
+
+const airbatURL = `http://airb.at/%s`
+
+// ErrInvalidID is returned by conversion functions when the given id is of zero value (0 or "")
+var ErrInvalidID = errors.New("invalid id")
+
+func toAirbat(id uint64) (string, error) {
+	// check input value
+	if id == 0 {
+		return "", ErrInvalidID
+	}
+
+	// put unsigned varint into []byte buffer
+	buf := make([]byte, 0, 8)
+	_ = binary.PutUvarint(buf, id)
+
+	// encode with base64 URL encoding
+	code := base64.URLEncoding.EncodeToString(buf)
+
+	// all done
+	return code, nil
+}
+
+func fromAirbat(code string) (uint64, error) {
+	// check input value
+	if code == "" {
+		return 0, ErrInvalidID
+	}
+
+	// base64 decode
+	buf, err := base64.URLEncoding.DecodeString(code)
+	if err != nil {
+		return 0, err
+	}
+
+	id, _ := binary.Uvarint(buf)
+	return id, nil
+}
+
+// UintToAirbatCode converts the given notice-id to a airbat-code
+func UintToAirbatCode(id uint64) (string, error) {
+	return toAirbat(id)
+}
+
+// UintToAirbatURL converts the given notice-id to a airbat url
+func UintToAirbatURL(id uint64) (string, error) {
+	code, err := toAirbat(id)
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf(airbatURL, code), nil
+}
+
+// AirbatCodeToUint converts the given airbat code to an notice-id
+func AirbatCodeToUint(code string) (uint64, error) {
+	return fromAirbat(code)
+}
---
+++ airbat/main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"fmt"
+	"github.com/GeertJohan/go.airbat"
+	"github.com/jessevdk/go-flags"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+const airbrakeNoticeURL = "http://airbrake.io/locate/%d"
+
+var (
+	tmplError    *template.Template
+	tmplRedirect *template.Template
+)
+
+func init() {
+	var err error
+	tmplError, err = template.New("error").Parse(`<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Airb.at - This is not the error you are looking for</title>
+		<meta name="description" content="Airb.at short urls for airbrake.io">
+		<meta name="author" content="Geert-Johan Riemer">
+	</head>
+	<body>
+		<center>
+			<h3>This is not the error you are looking for.</h3>
+			<p>
+				An error occurred. The given URL seems to be inavalid.<br/>
+				{{.Error}}
+			</p>
+		</center>
+	</body>
+</html>`)
+	if err != nil {
+		log.Printf("error parsing error template: %s\n", err)
+		os.Exit(1)
+	}
+
+	tmplRedirect, err = template.New("redirect").Parse(`<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Airb.at - You are being redirected</title>
+		<meta name="description" content="Airb.at short urls for airbrake.io">
+		<meta name="author" content="Geert-Johan Riemer">
+	</head>
+	<body>
+		<center>
+			<h3>Redirecting..</h3>
+			<p>
+				You are being redirected to: <a href="{{.URL}}" >{{.URL}}</a>
+			</p>
+		</center>
+	</body>
+</html>`)
+	if err != nil {
+		log.Printf("error parsing redirect template: %s\n", err)
+		os.Exit(1)
+	}
+}
+
+type dataError struct {
+	Error string
+}
+
+type dataRedirect struct {
+	URL template.URL
+}
+
+var options struct {
+	HTTPPort string `long:"port" default:"8321"`
+}
+
+func main() {
+	// parse flags
+	_, err := flags.Parse(&options)
+	if err != nil {
+		fmt.Printf("error parsing flags: %s\n", err)
+		os.Exit(1)
+	}
+
+	// set http handler on root request uri
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// get code from RequestURI
+		code := r.RequestURI[1:]
+		if len(code) == 0 {
+			http.Redirect(w, r, "https://github.com/GeertJohan/go.airbat", http.StatusTemporaryRedirect)
+			return
+		}
+
+		// convert code to notice id
+		id, err := airbat.AirbatCodeToUint(code)
+		if err != nil {
+			//++ TODO: use airbrake
+			log.Printf("could not convert to uint: %s\n", err)
+
+			// write error template
+			data := dataError{
+				Error: err.Error(),
+			}
+			w.WriteHeader(http.StatusBadRequest)
+			err = tmplError.Execute(w, data)
+			if err != nil {
+				//++ TODO: use airbrake
+				log.Printf("error executing error template: %s\n", err)
+				return
+			}
+			return
+		}
+
+		// create airbrake url
+		urlStr := fmt.Sprintf(airbrakeNoticeURL, id)
+
+		// write redirect header
+		w.Header().Set("Location", urlStr)
+		w.WriteHeader(http.StatusTemporaryRedirect)
+
+		// RFC2616 recommends that a short note "SHOULD" be included in the
+		// response because older user agents may not understand 301/307.
+		// Shouldn't send the response for POST or HEAD; that leaves GET.
+		if r.Method == "GET" {
+			// setup data for template
+			data := dataRedirect{
+				URL: template.URL(urlStr),
+			}
+			// execute template
+			err = tmplRedirect.Execute(w, data)
+			if err != nil {
+				//++ TODO: use airbrake
+				log.Printf("error writing redirect template: %s\n", err)
+			}
+		}
+	})
+
+	// start http server in goroutine
+	go func() {
+		err := http.ListenAndServe(":"+options.HTTPPort, nil)
+		if err != nil {
+			//++ TODO: use airbrake
+			log.Printf("error listening on port %s: %s\n", options.HTTPPort, err)
+			os.Exit(1)
+		}
+	}()
+
+	// wait forever
+	select {}
+}
